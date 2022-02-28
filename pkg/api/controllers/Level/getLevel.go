f3222c88277c29e810c2b72e59c55973406fabee
julychildx <100307596+julychildx@users.noreply.github.com>
2022-02-28 20:46:52 +0100

    Create getLevel.go

---
+++ pkg/api/controllers/Level/getLevel.go
@@ -0,0 +1,62 @@
+package level
+
+import (
+	db "github.com/cave/pkg/database"
+	"github.com/cave/pkg/helpers"
+	"github.com/cave/pkg/models"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (c Controller) GetSingle(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+
+	levelId, parseError := primitive.ObjectIDFromHex(id)
+	if parseError != nil {
+		return helpers.BadResponse(ctx, "Bad Request", parseError.Error())
+	}
+
+	collection := db.Instance.Database.Collection("level")
+
+	query := bson.D{{Key: "_id", Value: levelId}}
+	rawRecord := collection.FindOne(ctx.Context(), query)
+	record := &models.Level{}
+	rawRecord.Decode(record)
+
+	if rawRecord.Err() != nil {
+		return helpers.NotFoundResponse(ctx, "Data not found in database")
+	} else {
+		return helpers.CrudResponse(ctx, "Get", record)
+	}
+
+	db "github.com/cave/pkg/database"
+	"github.com/cave/pkg/helpers"
+	"github.com/cave/pkg/models"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (c Controller) GetSingle(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+
+	employeId, parseError := primitive.ObjectIDFromHex(id)
+	if parseError != nil {
+		return helpers.BadResponse(ctx, "Bad Request", parseError.Error())
+	}
+
+	collection := db.Instance.Database.Collection("level")
+
+	query := bson.D{{Key: "_id", Value: levelId}}
+	rawRecord := collection.FindOne(ctx.Context(), query)
+	record := &models.Level{}
+	rawRecord.Decode(record)
+
+	if rawRecord.Err() != nil {
+		return helpers.NotFoundResponse(ctx, "Data not found in database")
+	} else {
+		return helpers.CrudResponse(ctx, "Get", record)
+	}
+}
+
