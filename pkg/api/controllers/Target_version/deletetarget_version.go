2cce6189c3edf75f6dc8563e74ceb5ffec7d1c3e
julychildx <100307596+julychildx@users.noreply.github.com>
2022-02-28 16:00:49 +0100

    Create deletetarget_version.go

---
+++ pkg/api/controllers/Target_version/deletetarget_version.go
@@ -0,0 +1,48 @@
+package target_version.go
+
+import (
+	. "github.com/cave/pkg/api/controllers"
+	db "github.com/cave/pkg/database"
+	"github.com/cave/pkg/helpers"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (c Controller) DeleteSingle(ctx *fiber.Ctx) error {
+	// check data
+	id := ctx.Params("id")
+
+	target_versionId, parseError := primitive.ObjectIDFromHex(id)
+	if parseError != nil {
+		return helpers.BadResponse(ctx, "Bad Request", parseError.Error())
+	}
+
+	// get collection
+	collection := db.Instance.Database.Collection("target_version")
+
+	// check if the record is there
+	query := bson.D{{Key: "_id", Value: target_versionId}}
+	result, deleteError := collection.DeleteOne(ctx.Context(), &query)
+
+	if deleteError != nil {
+		return helpers.ServerResponse(ctx, deleteError.Error(), deleteError.Error())
+	}
+
+	// check if item was deleted
+	if result.DeletedCount < 1 {
+		return helpers.NotFoundResponse(ctx, "Data not found in database")
+	} else {
+		return helpers.CrudResponse(ctx, "Deleted", result)
+	}
+}
+
+func DeleteAll(ctx *fiber.Ctx) error {
+	// get collection
+	collection := db.Instance.Database.Collection("employe")
+
+	// check if the record is there
+	deleteResult := collection.Drop(ctx.Context())
+
+	return helpers.CrudResponse(ctx, "Deleted", deleteResult)
+}
