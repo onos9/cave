a96a481b3ea57eea1144f97b050ea1ae48e3c013
julychildx <100307596+julychildx@users.noreply.github.com>
2022-02-28 18:44:30 +0100

    Update and rename createtarget_group.go to createTargetGroup.go

--- pkg/api/controllers/Target_group/createTargetGroup.go
+++ pkg/api/controllers/Target_group/createTargetGroup.go
@@ -1,4 +1,4 @@
-package target_group
+package targetgroup
 
 import (
 	"github.com/asaskevich/govalidator"
@@ -12,15 +12,15 @@ import (
 
 func (c *Controller) CreateNew(ctx *fiber.Ctx) error {
 
-	collection := db.Instance.Database.Collection("target_group")
+	collection := db.Instance.Database.Collection("targetgroup")
 
 	// create a new record
-	target_group := new(models.Target_group)
-	target_group.CreatedAt = utils.MakeTimestamp()
-	target_group.UpdatedAt = utils.MakeTimestamp()
+	targetgroup := new(models.TargetGroup)
+	targetgroup.CreatedAt = utils.MakeTimestamp()
+	targetgroup.UpdatedAt = utils.MakeTimestamp()
 
-	if errors := ctx.BodyParser(target_group); errors != nil {
-		_, err := govalidator.ValidateStruct(target_group)
+	if errors := ctx.BodyParser(targetgroup); errors != nil {
+		_, err := govalidator.ValidateStruct(targetgroup)
 
 		if err != nil {
 			return helpers.ServerResponse(ctx, err.Error(), err)
@@ -28,15 +28,15 @@ func (c *Controller) CreateNew(ctx *fiber.Ctx) error {
 
 		return helpers.ServerResponse(ctx, errors.Error(), errors)
 	} else {
-		if result, errs := collection.InsertOne(ctx.Context(), target_group); errs != nil {
+		if result, errs := collection.InsertOne(ctx.Context(), targetgroup); errs != nil {
 			return helpers.ServerResponse(ctx, errs.Error(), errs.Error())
 		} else {
 			filter := bson.D{{Key: "_id", Value: result.InsertedID}}
 			createdRecord := collection.FindOne(ctx.Context(), filter)
-			createdtarget_group := &models.Target_group{}
-			createdRecord.Decode(createdtarget_group)
+			createdtargetgroup := &models.TargetGroup{}
+			createdRecord.Decode(createdtargetgroup)
 
-			return helpers.CrudResponse(ctx, "Create", createdtarget_group)
+			return helpers.CrudResponse(ctx, "Create", createdtargetgroup)
 		}
 	}
 }
