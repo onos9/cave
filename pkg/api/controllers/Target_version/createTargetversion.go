768ae4a8a091f6ff6ecd1d084bae3a0d011c4b74
julychildx <100307596+julychildx@users.noreply.github.com>
2022-02-28 18:55:12 +0100

    Update and rename createtarget_version.go to createTargetversion.go

--- pkg/api/controllers/Target_version/createTargetversion.go
+++ pkg/api/controllers/Target_version/createTargetversion.go
@@ -1,4 +1,4 @@
-package target_version
+package targetversion
 
 import (
 	"github.com/asaskevich/govalidator"
@@ -12,15 +12,15 @@ import (
 
 func (c *Controller) CreateNew(ctx *fiber.Ctx) error {
 
-	collection := db.Instance.Database.Collection("target_version")
+	collection := db.Instance.Database.Collection("targetversion")
 
 	// create a new record
-	target_version := new(models.Target_version)
-	target_version.CreatedAt = utils.MakeTimestamp()
-	target_version.UpdatedAt = utils.MakeTimestamp()
+	targetversion := new(models.TargetVersion)
+	targetversion.CreatedAt = utils.MakeTimestamp()
+	targetversion.UpdatedAt = utils.MakeTimestamp()
 
-	if errors := ctx.BodyParser(target_version); errors != nil {
-		_, err := govalidator.ValidateStruct(target_version)
+	if errors := ctx.BodyParser(targetversion); errors != nil {
+		_, err := govalidator.ValidateStruct(targetversion)
 
 		if err != nil {
 			return helpers.ServerResponse(ctx, err.Error(), err)
@@ -28,15 +28,15 @@ func (c *Controller) CreateNew(ctx *fiber.Ctx) error {
 
 		return helpers.ServerResponse(ctx, errors.Error(), errors)
 	} else {
-		if result, errs := collection.InsertOne(ctx.Context(), target_version); errs != nil {
+		if result, errs := collection.InsertOne(ctx.Context(), targetversion); errs != nil {
 			return helpers.ServerResponse(ctx, errs.Error(), errs.Error())
 		} else {
 			filter := bson.D{{Key: "_id", Value: result.InsertedID}}
 			createdRecord := collection.FindOne(ctx.Context(), filter)
-			createdtarget_version := &models.Target_version{}
-			createdRecord.Decode(createdtarget_version)
+			createdtargetversion := &models.TargetVersion{}
+			createdRecord.Decode(createdtargetversion)
 
-			return helpers.CrudResponse(ctx, "Create", createdtarget_version)
+			return helpers.CrudResponse(ctx, "Create", createdtargetversion)
 		}
 	}
 }
