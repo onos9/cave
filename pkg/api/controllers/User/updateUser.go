68afdab80c32c575eda6de72f24874e914db5ade
julychildx <100307596+julychildx@users.noreply.github.com>
2022-02-28 10:49:45 +0100

    Create updateUser.go

---
+++ pkg/api/controllers/User/updateUser.go
@@ -0,0 +1,53 @@
+package user
+
+import (
+	"time"
+
+	db "github.com/cave/pkg/database"
+	"github.com/cave/pkg/helpers"
+	"github.com/cave/pkg/models"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (c Controller) UpdateSingle(ctx *fiber.Ctx) error {
+
+	id := ctx.Params("id")
+	user := new(models.User)
+	userId, parseError := primitive.ObjectIDFromHex(id)
+	if parseError != nil {
+		return helpers.BadResponse(ctx, "Bad Request", parseError.Error())
+	}
+
+	parsingError := ctx.BodyParser(user)
+	if parsingError != nil {
+		helpers.ServerResponse(ctx, parsingError.Error(), parsingError.Error())
+	}
+
+	collection := db.Instance.Database.Collection("user")
+
+	// check if the record is there
+	query := bson.D{{Key: "_id", Value: userId}}
+	rawRecord := collection.FindOne(ctx.Context(), query)
+	record := &models.User{}
+	rawRecord.Decode(record)
+
+	// update the record
+	update := bson.D{
+		{Key: "$set",
+			Value: bson.D{
+				{Key: "name", Value: user.Name},
+				{Key: "salary", Value: user.Salary},
+				{Key: "age", Value: user.Age},
+				{Key: "updatedAt", Value: time.Now()},
+			},
+		},
+	}
+	result, updateError := collection.UpdateOne(ctx.Context(), query, update)
+	if updateError != nil {
+		return helpers.ServerResponse(ctx, updateError.Error(), updateError.Error())
+	}
+
+	return helpers.CrudResponse(ctx, "Update", result)
+}
